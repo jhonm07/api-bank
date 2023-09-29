b198c8e736235c61fe410f5ade445be20f14478f
john <john.munoz@detektor.com.co>
2023-09-29 00:48:33 -0500

    fist commit

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    try:
+        current_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        return render_template('index.html', message='Bienvenido a mi aplicación', datetime=current_datetime)
+    except Exception as e:
+        return str(e)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
